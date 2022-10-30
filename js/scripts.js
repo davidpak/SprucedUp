8fc23828ee401951bc29cefb54ade179d27c6128
davidpak <sungh1404@gmail.com>
2022-10-29 20:01:14 -0700

    Added progress bar, tree, colors, and animation

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,31 @@
 //
 // Scripts
 // 
+const progress = document.querySelector(".progress-done")
+// EDIT
+const input = document.querySelector(".input")
+const maxInput = document.querySelector(".maxInput")
+let finalValue = 0
+let max = 0;
+
+function changeWidth() {
+    progress.style.width = `${(finalValue / max) * 100}%`;
+    progress.innerText = `${Math.ceil((finalValue / max) * 100)}%`;
+}
+
+input.addEventListener("keyup", function () {
+    finalValue = parseInt(input.value, 10);
+    console.log("finalValue", finalValue);
+    changeWidth();
+});
+
+maxInput.addEventListener("keyup", function () {
+    max = parseInt(maxInput.value, 10);
+    console.log("max", max);
+    changeWidth();
+});
+
+
 
 window.addEventListener('DOMContentLoaded', event => {
 
